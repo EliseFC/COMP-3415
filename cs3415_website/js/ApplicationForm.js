98386647f5d00bcb98e633879451f0acf5a56e22
Elise C <elise@Elises-MacBook-Pro.local>
2016-11-21 16:09:10 -0500

    more about application

--- cs3415_website/js/ApplicationForm.js
+++ cs3415_website/js/ApplicationForm.js
@@ -1,22 +1,13 @@
-function updateApplication(){
-	$("#").empty();
-	
-	getBuildings(function(response){
-		if(!response.success){
-			console.log("error getting buildings");
-		}else{
-			buildings = response.buildings;
-			buildings.forEach(function(entry){
-				$("#buildings").append('<li class="style=ui-widget-content" id="'+entry.id+'">'+entry.name+': '+entry.type+'</li>');
-			});
-			console.log("**Sucessfully fetched buildings!**");
-		}
-	});
-}
-
 	
 $(document).ready(function(){
     $("#saveApplication").click(function(){
-        alert("You Application has down!");
+        alert("You Application has down!"+$(".selected").attr('id'));
     });
+	
+	$("#main_list li").click(function(){
+		$("#main_list li").removeClass("selected");
+		$(this).addClass("selected").siblings();
+	});
+	
+	
 });
