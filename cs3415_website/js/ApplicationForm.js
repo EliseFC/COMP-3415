7156217f1aee8de92a6f860ac67d429593464d51
Elise <fchen3@lakeheadu.ca>
2016-11-23 13:07:24 -0500

    application form

--- cs3415_website/js/ApplicationForm.js
+++ cs3415_website/js/ApplicationForm.js
@@ -1,14 +1,38 @@
 $(document).ready(function(){
 	var buildings;	
-    $("#saveApplication").click(function(){
-        addReqs(student_id,"hello",building[0].id, function(response) {
-			if(!response.success){
+	var user;
+	var student=JSON.parse(sessionStorage.getItem("studentSession"));
+    
+	
+	getBuildings(function(response){
+		if(!response.success){
 			console.log("error getting buildings"+response.error_message);
-			}else{
+		}else{
 			buildings = response.buildings;
 			console.log("**Sucessfully fetched buildings!**");
-			}	
-        });
+		}
+	});
+	
+	$("#saveApplication").click(function(){
+		console.log(student.username);
+		getUserByEmail(student.username, function(response){
+			if(!response.success){
+				console.log("**Error get user inform**"+response.error_message);
+			}else{
+				console.log("**Successfully get user inform**");
+				user = response.user;
+			
+				addHousingRequest(user.user_id,$("#id_specialReqs").val(),buildings[0].id, function(response) {
+					if(!response.success){
+					console.log("error getting buildings"+response.error_message);
+					}else{
+					buildings = response.buildings;
+					console.log("**Sucessfully fetched buildings!**");
+					window.location.href = 'shome.html';
+					}	
+		        });
+			}
+			});
 	});
 	
 	$(".page_section li").click(function(){
